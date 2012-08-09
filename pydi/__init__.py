5fffb21040fb46a86b7dfbc692116a807389a009
Adam Venturella <aventurella@gmail.com>
2012-08-09 06:17:38 -0700

    setup

--- pydi/__init__.py
+++ pydi/__init__.py
@@ -1,1 +1,16 @@
+"""
+pydi - Dependency Injection Container
+~~~~~~~~~~~~
+
+:copyright: (c) 2012 by Adam Venturella.
+:license: ISC, see LICENSE for more details.
+
+"""
+__title__ = 'pydi'
+__version__ = '0.1'
+__author__ = 'Adam Venturella'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2012 Adam Venturella'
+
+
 from .container import Container
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+packages = [
+    'pydi'
+]
+
+requires = []
+
+setup(
+    name='pydi',
+    version='0.1',
+    description='Little Dependency Injection Container',
+    long_description=readme,
+    author='Adam Venturella',
+    author_email='aventurella@gmail.com',
+    url='http://github.com/aventurella/pydi',
+    license=license,
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    package_dir={'pydi': 'pydi'},
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+    ),
+
+)
